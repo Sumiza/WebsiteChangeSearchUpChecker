af91b39235e7636953640bc5698ab8466d6bddb1
Sumiza <64884509+Sumiza@users.noreply.github.com>
2022-03-16 02:06:10 -0400

    Update webcheck.py

--- webcheck.py
+++ webcheck.py
@@ -59,12 +59,13 @@ class Parsedb(object):
     def checkport(self):
         self.getport()
         if self.statuscode == 0:
-            self.message = f"Port is OPEN after {int(time())-int(self.checked)} seconds"
+            self.message = "Port is OPEN"
             self.trycount = 0
         else:
             self.message = "Port is Closed"
             self.trycount += 1
-
+        if self.checked != 0:
+            self.message = self.message+f" after {sectotext(int(time())-int(self.checked))}"
         if self.new != self.last:
             if self.trycount == self.trytrigger or self.trycount == 0:
                 self.sendmess()
@@ -81,6 +82,7 @@ class Parsedb(object):
             if self.keyword in self.new:
                 self.message = "- Found"
             if self.message != self.last:
+                self.message = self.message+f"\n\n {sectotext(int(time())-int(self.checked))} Since last change"
                 self.sendmess()
             db.execute("UPDATE webcheck SET last = ?, checked = ?, count = 1 WHERE target = ? AND checktype = 'word'",
                         (self.message,int(time()),self.target))
@@ -91,7 +93,7 @@ class Parsedb(object):
             if self.last != self.new:
                 diff = ndiff(self.last.splitlines(), self.new.splitlines())
                 self.message = [l for l in diff if l.startswith("+ ") or l.startswith("- ")]
-                self.message = '\n'.join(self.message)
+                self.message = '\n'.join(self.message)+f"\n\n{sectotext(int(time())-int(self.checked))} Since last change"
                 if self.checked == 0:
                     self.message = "Website Added"
                 self.sendmess()
@@ -101,12 +103,13 @@ class Parsedb(object):
     def websitestatus(self) -> None:
         self.getwebsite()
         if 400 >= self.statuscode >= 200:
-            self.message = f"Is ONLINE after {int(time())-int(self.checked)} seconds"
+            self.message = "Is ONLINE"
             self.trycount = 0
         else:
             self.trycount += 1
             self.message = "Is OFFLINE"
-
+        if self.checked != 0:
+            self.message = self.message+f" after {sectotext(int(time())-int(self.checked))}"
         if str(self.statuscode) != self.last:
             if self.trycount == self.trytrigger or self.trycount == 0:
                 self.sendmess()
@@ -154,6 +157,66 @@ class Parsedb(object):
                 self.websitestatus()
             elif self.checktype == 'port':
                 self.checkport()
+
+def sectotext(seconds:int,restype:str=""):
+    """
+    Converts seconds to human readable text or tuple
+    
+    Args:
+        seconds:    amount of seconds for it to process,
+                    can be float but will be changed to int,
+                    can be negative but will be changed to positive.
+                    
+        restype:
+                default:    skips any part of the response that is 0.
+                    print(sectotext(12069123))
+                    4 Months, 18 Days, 32 Minutes, 3 Seconds
+
+                showzeros:  trims any part before the first not 0.
+                    print(sectotext(-12069123,"showzeros"))
+                    4 Months, 18 Days, 0 Hours, 32 Minutes, 3 Seconds
+                
+                showall:    shows all parts.
+                    print(sectotext(12069123,"showall"))
+                    0 Years, 4 Months, 18 Days, 0 Hours, 32 Minutes, 3 Seconds
+                
+                rawtuple:   returns a 6 part tuple.
+                    print(sectotext(12069123,"rawtuple"))
+                    (0, 4, 18, 0, 32, 3)
+    """
+    
+    seconds = int(seconds)
+    if seconds < 0:
+        seconds = seconds * -1
+    years, seconds = divmod(seconds,31536000)
+    months, seconds = divmod(seconds,2628000)
+    days, seconds = divmod(seconds,86400)
+    hours, seconds = divmod(seconds,3600)
+    minutes, seconds = divmod(seconds,60)
+
+    def plural(n:str):
+        if n != 1:
+            return "s"
+        return ""
+        
+    p = [f"{years} Year{plural(years)}",
+        f"{months} Month{plural(months)}",
+        f"{days} Day{plural(days)}", 
+        f"{hours} Hour{plural(hours)}", 
+        f"{minutes} Minute{plural(minutes)}", 
+        f"{seconds} Second{plural(seconds)}"]
+    
+    if restype == "showzeros":
+        for loc,res in enumerate(p):
+            if res[0] != "0":
+                return ", ".join(p[loc:len(p)])
+    elif restype == "showall":
+        return ", ".join([ res for res in p])
+    elif restype == "rawtuple":
+        return (years,months,days,hours,minutes,seconds)
+    else:
+        return ", ".join([ res for res in p if res[0] != "0" ])
+        
 
 def amionlinedns(hostlist:list = ["1.1.1.1","8.8.8.8","9.9.9.9"]):
     """
