291d559f6a4ca4d8b11a2a1569804250407d2245
Sumiza <64884509+Sumiza@users.noreply.github.com>
2022-03-21 01:59:22 -0400

    Update webchecksetup.py

--- webchecksetup.py
+++ webchecksetup.py
@@ -1,279 +1,106 @@
-from time import sleep, time
-from requests import get, head
-from bs4 import BeautifulSoup
-from threading import Thread
-from difflib import ndiff
-from apisender import send
-import socket
+
 import sqlite3
+db = sqlite3.connect("onchecker.db")
+db.execute("PRAGMA journal_mode = 'WAL'")
+db.execute("""CREATE TABLE IF NOT EXISTS webcheck 
+            (target TEXT,
+            checktype TEXT,
+            checked INTEGER DEFAULT 0,
+            count INTEGER DEFAULT 1,
+            trigger INTEGER DEFAULT 1,
+            last TEXT DEFAULT 0,
+            keyword TEXT,
+            trycount INTEGER DEFAULT 0,
+            trytrigger INTEGER DEFAULT 0)
+            """)
+def addinfo():
+    print("Adding to database")
+    db.execute("INSERT OR IGNORE INTO webcheck (target,checktype,trigger,keyword,trytrigger) VALUES(?, ?, ?, ?, ?)",
+                (p["target"],p["checktype"],p["trigger"],p["keyword"],p["trytrigger"]))
+    db.commit()
+select = -1
+while select != 0:
+    p = {
+        "target" : "",
+        "checktype" : "",
+        "trigger" : 1,
+        "keyword" : "",
+        "trytrigger" : 1
+    }
+    if select == -1:
+        print("\n1. Add new website")
+        print("2. List websites")
+        print("3. Remove website")
+        print("4. Clear saved information for each domain")
+        print("0. Exit")
+        select = int(input("Please select an option: "))
 
+    if select == 1:
+        print("\n1. Website Text Checker")
+        print("2. Website Change Checker")
+        print("3. Website Online Checker")
+        print("4. Open Port Checker")
+        print("0. Exit")
+        select = int(input("Please select an option: "))
+        if select == 1:
+            print("\n"+"="*10+"Website Text Checker"+"="*10)
+            p["checktype"] = "word"
+            p["target"] = input("Target Domain, protocol(http or https required) ex: https://www.google.com/\n")
+            p["keyword"] = input("Keyword, word or words to look for (case sensitive) ex: Words to look for\n")
+            p["trigger"] = input("Check every runs, 1 is default at 60sec, ex: 1\n")
+            yn = input(f"Check {p['target']} look for {p['keyword']} every {p['trigger']} Y/N" ).casefold()
 
-class Parsedb(object):
-
-    def __init__(self,full:tuple) -> None:
-        self.target = full[0]
-        self.checktype = full[1]
-        self.checked = full[2]
-        self.count = full[3]
-        self.trigger = full[4]
-        self.last = full[5]
-        self.keyword = full[6]
-        self.trycount = full[7]
-        self.trytrigger = full[8]
-        self.message = ""
-        self.title = ""
-        self.statuscode = 0
-        self.new = ""
-
-    def getwebsite(self) -> None:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-        try:
-            if self.checktype == "word" or self.checktype == "change":
-                r = get(self.target,headers=headers,timeout=5)
-                soup = BeautifulSoup(r.text,'html.parser')
-                if soup.title:
-                    self.title = soup.title.string
-                else:
-                    self.title = self.target
-                self.new = "".join([s for s in soup.get_text().strip().splitlines(True) if s.strip()])
-                self.statuscode = r.status_code                  
-            elif self.checktype == "online":
-                r = head(self.target,headers=headers,timeout=5)
-                self.statuscode = r.status_code
-        except:
-            self.statuscode = 404
-
-    def getport(self) -> None:
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
-                so.settimeout(5)
-                target = self.target.split(":")
-                self.statuscode = so.connect_ex((socket.gethostbyname(target[0]), int(target[1])))
-                if self.statuscode == 0:
-                    self.new = "Port Open"
-                else:
-                    self.new = "Port Closed"
-        except:
-            self.new = "Error"
-
-    def checkport(self):
-        self.getport()
-        lasttrycount = self.trycount
-        if self.statuscode == 0:
-            self.message = "Port is OPEN"
-            self.trycount = 0
-        else:
-            self.message = "Port is Closed"
-            self.trycount += 1
-        if self.checked != 0:
-            self.message = self.message+f" after {sectotext(int(time())-int(self.checked))}"
-        if self.trycount <= self.trytrigger:
-            if self.new != self.last:
-                if self.trycount == self.trytrigger or self.trycount == 0:
-                    self.sendmess()
-                    self.checked = int(time())
-                else:
-                    self.new = self.last
-            if lasttrycount != self.trycount:  
-                print("writing to port")
-                print(self.new,self.trycount,int(time()),self.target)
-                db.execute("UPDATE webcheck SET last = ?, trycount = ?, checked = ?, count = 1 WHERE target = ? AND checktype = 'port'",
-                            (self.new,self.trycount,self.checked,self.target))
-
-
-    def websitetextcheck(self) -> None:
-        self.getwebsite()
-        if 400 >= self.statuscode >= 200:
-            self.message = "- Not Found"
-            if self.keyword in self.new:
-                self.message = "- Found"
-            if self.message != self.last:
-                self.message = self.message+f"\n\n {sectotext(int(time())-int(self.checked))} Since last change"
-                self.sendmess()
-            db.execute("UPDATE webcheck SET last = ?, checked = ?, count = 1 WHERE target = ? AND checktype = 'word'",
-                        (self.message,int(time()),self.target))
-
-    def websitechange(self) -> None:
-        self.getwebsite()
-        if 400 >= self.statuscode >= 200:
-            if self.last != self.new:
-                diff = ndiff(self.last.splitlines(), self.new.splitlines())
-                self.message = [l for l in diff if l.startswith("+ ") or l.startswith("- ")]
-                self.message = '\n'.join(self.message)+f"\n\n{sectotext(int(time())-int(self.checked))} Since last change"
-                if self.checked == 0:
-                    self.message = "Website Added"
-                self.sendmess()
-            db.execute("UPDATE webcheck SET last = ?, checked = ?, count = 1 WHERE target = ? AND checktype = 'change'",
-                        (self.new,int(time()),self.target))
-    
-    def websitestatus(self) -> None:
-        self.getwebsite()
-        lasttrycount = self.trycount
-        if 400 >= self.statuscode >= 200:
-            self.message = "Is ONLINE"
-            self.trycount = 0
-        else:
-            self.trycount += 1
-            self.message = "Is OFFLINE"
-        if self.checked != 0:
-            self.message = self.message+f" after {sectotext(int(time())-int(self.checked))}"
-
-        if self.trycount <= self.trytrigger:
-            if str(self.statuscode) != self.last:
-                if self.trycount == self.trytrigger or self.trycount == 0:
-                    self.sendmess()
-                    self.checked = int(time())
-                else:
-                    self.statuscode = self.last
-            if lasttrycount != self.trycount:    
-                print("writing to status")
-                print(self.statuscode,self.trycount,int(time()),self.target)
-                db.execute("UPDATE webcheck SET last = ?, trycount = ?, checked = ?, count = 1 WHERE target = ? AND checktype = 'online'",
-                            (self.statuscode,self.trycount,self.checked,self.target))
-
-    def sendmess(self):
-        """
-            Connect whatever API to send messages
-        """
-        if self.checktype == 'word':
-            self.mess = f"Website: {self.target}\n{self.title}\n{self.keyword} {self.message}\n"+"="*80
-            subject = "Website Word Finder"
-        elif self.checktype == 'change':
-            self.mess = f"Website: {self.target}\n{self.title}\n{self.message}\n"+"="*80
-            subject = "Website Changed"
-        elif self.checktype == 'online':
-            self.mess = f"Website: {self.target}\n{self.message}\nStatus Code:{self.statuscode}\n"+"="*80
-            subject = "Website Online Checker"
-        elif self.checktype == 'port':
-            self.mess = f"Port: {self.target}\n{self.message}\n"+"="*80
-            subject = "Port Checker"
-        send(bodytext=self.mess,subject=subject,sendby="discord")
-        print(subject,"\n",self.mess)
-
-    def runornot(self) -> bool:
-        if self.count >= self.trigger:
-            # print("returning true for ",self.target,self.count,self.trigger)
-            return True
-        else:
-            # print("returning false for ",self.target,self.count,self.trigger)
-            db.execute("UPDATE webcheck SET count = ? WHERE target = ? AND checktype = ?",
-                        (self.count+1,self.target,self.checktype))
-            return False
-
-    def typeselect(self) -> None:
-        if self.runornot():
-            if self.checktype == 'word':
-                self.websitetextcheck()
-            elif self.checktype == 'change':
-                self.websitechange()
-            elif self.checktype == 'online':
-                self.websitestatus()
-            elif self.checktype == 'port':
-                self.checkport()
-
-def sectotext(seconds:int,restype:str=""):
-    """
-    Converts seconds to human readable text or tuple
-    
-    Args:
-        seconds:    amount of seconds for it to process,
-                    can be float but will be changed to int,
-                    can be negative but will be changed to positive.
-                    
-        restype:
-                default:    skips any part of the response that is 0.
-                    print(sectotext(12069123))
-                    4 Months, 18 Days, 32 Minutes, 3 Seconds
-
-                showzeros:  trims any part before the first not 0.
-                    print(sectotext(-12069123,"showzeros"))
-                    4 Months, 18 Days, 0 Hours, 32 Minutes, 3 Seconds
-                
-                showall:    shows all parts.
-                    print(sectotext(12069123,"showall"))
-                    0 Years, 4 Months, 18 Days, 0 Hours, 32 Minutes, 3 Seconds
-                
-                rawtuple:   returns a 6 part tuple.
-                    print(sectotext(12069123,"rawtuple"))
-                    (0, 4, 18, 0, 32, 3)
-    """
-    
-    seconds = int(seconds)
-    if seconds < 0:
-        seconds = seconds * -1
-    years, seconds = divmod(seconds,31536000)
-    months, seconds = divmod(seconds,2628000)
-    days, seconds = divmod(seconds,86400)
-    hours, seconds = divmod(seconds,3600)
-    minutes, seconds = divmod(seconds,60)
-
-    def plural(n:str):
-        if n != 1:
-            return "s"
-        return ""
+        elif select == 2:
+            p["checktype"] = "change"
+            p["target"] = input("Target Domain, protocol(http or https required) ex: https://www.google.com/\n")
+            p["trigger"] = input("Check every runs, 1 is default at 60sec, ex: 1\n")
+            yn = input(f"Check {p['target']} every {p['trigger']} Y/N" ).casefold()
         
-    p = [f"{years} Year{plural(years)}",
-        f"{months} Month{plural(months)}",
-        f"{days} Day{plural(days)}", 
-        f"{hours} Hour{plural(hours)}", 
-        f"{minutes} Minute{plural(minutes)}", 
-        f"{seconds} Second{plural(seconds)}"]
-    
-    if restype == "showzeros":
-        for loc,res in enumerate(p):
-            if res[0] != "0":
-                return ", ".join(p[loc:len(p)])
-    elif restype == "showall":
-        return ", ".join([ res for res in p])
-    elif restype == "rawtuple":
-        return (years,months,days,hours,minutes,seconds)
-    else:
-        return ", ".join([ res for res in p if res[0] != "0" ])
-        
+        elif select == 3:
+            p["checktype"] = "online"
+            p["target"] = input("Target Domain, protocol(http or https required) ex: https://www.google.com/\n")
+            p["trigger"] = input("Check every runs, 1 is default at 60sec, ex: 1\n")
+            p["trytrigger"] = input("Notify after how many tries come back as offline, ex: 1\n")
+            yn = input(f"Check {p['target']} every {p['trigger']}, Notify after {p['trytrigger']} Y/N" ).casefold()
 
-def amionlinedns(hostlist:list = ["1.1.1.1","8.8.8.8","9.9.9.9"]):
-    """
-    Returns True if can connect to any of the dns server in hostlist.
-    Returns False if cant connect.
-    Args:
-        hostlist:list default is cloudflare, google, quad9
-    """
-    for host in hostlist:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
-            so.settimeout(5)
-            response = so.connect_ex((host,53))
-            if response == 0:
-                return True
-    return False
+        elif select == 4:
+            p["checktype"] = "port"
+            p["target"] = input("Target IP or FQDN with port number ex: google.com:80 or 8.8.8.8:53\n")
+            p["trigger"] = input("Check every runs, 1 is default at 60sec, ex: 1\n")
+            p["trytrigger"] = input("Notify after how many tries come back as offline, ex: 1\n")
+            yn = input(f"Check {p['target']} every {p['trigger']}, Notify after {p['trytrigger']} Y/N" ).casefold()
 
-def checkall(waitsleep:int = 60,threading:bool = False):
-    threads = []
-    starttime = time()
-    if amionlinedns():
-        for i in db.execute("SELECT DISTINCT checktype from webcheck").fetchall():
-            for j in db.execute(f"SELECT * FROM webcheck WHERE checktype = '{i[0]}'").fetchall():
-                check = Parsedb(j)
-                if threading:
-                    t = Thread(target=check.typeselect)
-                    threads.append(t)
-                    t.start()
-                else:
-                    check.typeselect()
-        if threading:
-            for i in threads:
-                i.join()
-        db.commit() 
-    db.close()
-    sleeptimer(starttime,waitsleep)
-    print(time()-starttime)
+        if yn == "y":
+            addinfo()
+        select = -1
 
-def sleeptimer(starttime:float,stimer:float):
-    stimer = starttime-time()+stimer
-    if stimer > 0:
-        sleep(stimer)
+    elif select == 2:
+        print("\n---Text Checker--")
+        for i in db.execute("SELECT target,keyword,trigger FROM webcheck WHERE checktype = 'word'").fetchall():
+            print(*i)
+        print("\n---Change Website---")
+        for i in db.execute("SELECT target,trigger FROM webcheck WHERE checktype = 'change'").fetchall():
+            print(*i)
+        print("\n---Uptime Checker---")
+        for i in db.execute("SELECT target,trytrigger,trigger FROM webcheck WHERE checktype = 'online'").fetchall():
+            print(*i)
+        print("\n---Port Checker---")
+        for i in db.execute("SELECT target,trytrigger,trigger FROM webcheck WHERE checktype = 'port'").fetchall():
+            print(*i)
+        select = -1
 
-if __name__ == '__main__':
-    while True:
-        db = sqlite3.connect("onchecker.db",check_same_thread=False)
-        db.execute("PRAGMA journal_mode = 'WAL'")
-        checkall()
+    elif select == 3:
+        print("Select what domain to delete")
+        print("word = Text Checker | change = Change Website | online = Uptime Checker | port = Port Checker")
+        print("ex: online https://www.google.com")
+        p = input('0 to exit\n')
+        if p:
+            p = p.split(" ")
+            db.execute("DELETE FROM webcheck WHERE checktype = ? AND target = ?",(p[0],p[1]))
+            db.commit()
+        select = -1
+    elif select == 4:
+        print("Resetting all saved information for websites")
+        db.execute('UPDATE webcheck SET last = 0, checked = 0, count = 0, trycount = 0')
+        db.commit()
+        select = -1
